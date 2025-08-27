438a6be24516545ae8297c0bcc6dc2e9ea46ecb4
ephraim <ephraims.818@gmail.com>
2025-08-28 02:14:46 +0600

    REACT--ULTIMATE-REACT---STATE-EVENTS-AND-FORMS - 02. Handling Events

--- steps/src/App.js
+++ steps/src/App.js
@@ -6,6 +6,12 @@ const messages = [
 
 function App() {
     const step = 1;
+    function handlePrevious() {
+        alert("Previous");
+    }
+    function handleNext() {
+        alert("Next");
+    }
 
     return (
         <div className="steps">
@@ -14,15 +20,23 @@ function App() {
                 <div className={`${step >= 2 ? "active" : ""}`}>2</div>
                 <div className={`${step >= 3 ? "active" : ""}`}>3</div>
             </div>
+
             <p className="message">
                 Step {step}: {messages[step - 1]}
             </p>
+
             <div className="buttons">
-                <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
-                    previous
+                <button
+                    style={{ backgroundColor: "#7950f2", color: "#fff" }}
+                    onClick={handlePrevious}
+                >
+                    Previous
                 </button>
-                <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
-                    next
+                <button
+                    style={{ backgroundColor: "#7950f2", color: "#fff" }}
+                    onClick={handleNext}
+                >
+                    Next
                 </button>
             </div>
         </div>
