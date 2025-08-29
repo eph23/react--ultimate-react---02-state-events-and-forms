4cb44e87e40a8c6fd3d47ffdf556854083f8ba71
ephraim <ephraims.818@gmail.com>
2025-08-29 17:39:23 +0600

    REACT--ULTIMATE-REACT---STATE-EVENTS-AND-FORMS - 05. Updating State Based on Current State

--- steps/src/App.js
+++ steps/src/App.js
@@ -11,11 +11,11 @@ function App() {
     const [isOpen, setIsOpen] = useState(true);
 
     function handlePrevious() {
-        if (step > 1) setStep(step - 1);
+        if (step > 1) setStep((s) => s - 1);
     }
 
     function handleNext() {
-        if (step < 3) setStep(step + 1);
+        if (step < 3) setStep((s) => s + 1);
     }
 
     return (
@@ -23,7 +23,7 @@ function App() {
             <button
                 className="close"
                 onClick={() => {
-                    setIsOpen(!isOpen);
+                    setIsOpen((is) => !is);
                 }}
             >
                 &times;
